c55cf1315da6b6aca7dd65ec485f7cd8b9204732
NirB94 <92429046+NirB94@users.noreply.github.com>
2023-08-13 17:00:13 +0300

    Add files via upload

--- hw3_313580920.py
+++ hw3_313580920.py
@@ -1,19 +1,20 @@
 import pandas as pd
 import numpy as np
 from statsmodels.stats.multitest import fdrcorrection
+from scipy.stats import linregress as lrs
 
 import hw2_regression as reg
 
 
 def filtering(genotypes, strains):
     relevant_strains = [column for column in genotypes.columns if column in strains]
-    columns = list(genotypes.columns)[:4]
+    columns = list(genotypes.columns)[:3]
     columns.extend(relevant_strains)
     final_genotypes = genotypes[columns]
     drop_indices = []
-    for i in range(len(final_genotypes)-1):
-        if (final_genotypes.iloc[i, 4:]).equals(final_genotypes.iloc[i+1, 4:]):
-            drop_indices.append(i+1)
+    for i in range(len(final_genotypes) - 1):
+        if (final_genotypes.iloc[i, 3:]).equals(final_genotypes.iloc[i + 1, 3:]):
+            drop_indices.append(i + 1)
     final_genotypes = final_genotypes.drop(drop_indices)
     return final_genotypes
 
@@ -32,21 +33,22 @@ def mean_data(table):
 
 def association_model(genotypes_df, expression_df, alpha=0.05):
     relevant_cols = [col for col in genotypes_df.columns if col in expression_df.columns]
-    genotypes_df = genotypes_df[genotypes_df.columns[:4].to_list() + relevant_cols]
+    genotypes_df = genotypes_df[genotypes_df.columns[:3].to_list() + relevant_cols]
     strains_dict = dict()
     for i in range(len(expression_df)):
-        p_vals = []
+        p_vals_list = []
         phe = expression_df.iloc[i]
         for index in genotypes_df.index:
             gen = genotypes_df.loc[index]
-            p_vals.append(reg.regression(phe, gen, False))
-        strains_dict[phe['data']] = p_vals
+            non_others_indexes = gen[(((gen == 'B') | (gen == 'D')) | (gen == 'H'))].index
+            gen, phe = gen[non_others_indexes], phe[non_others_indexes].astype(float)
+            gen = gen.map({'B': 2, 'H': 1, 'D': 0}).astype(int)
+            p_vals_list.append(lrs(gen, phe)[3])
+        strains_dict[phe['data']] = p_vals_list
     raw_mat = pd.DataFrame(strains_dict)
     raw_mat = raw_mat.T
-    print(raw_mat.shape)
     raw_mat.index = expression_df['data']
     raw_mat.columns = genotypes_df.index
-    raw_mat.drop(raw_mat.columns[0], axis=1, inplace=True)
     stacked = raw_mat.stack()
     stacked = pd.Series(fdrcorrection(stacked, 0.05)[1], index=stacked.index)
     final_mat = stacked.unstack()
@@ -55,9 +57,6 @@ def association_model(genotypes_df, expression_df, alpha=0.05):
     final_mat = final_mat[final_mat['min_p_val'] < alpha]
     final_mat.drop('min_p_val', axis='columns', inplace=True)
     return final_mat
-
-
-
 
 
 if __name__ == '__main__':
@@ -72,4 +71,3 @@ if __name__ == '__main__':
 
     locations_file = "MGI_Coordinates.Build37.rpt.txt"
     locations = pd.read_csv(locations_file, sep='\t', header=0)
-
